f99f169dbc5038df071697df728bf67d844f7433
Moetaz-doghman <61354236+Moetaz-doghman@users.noreply.github.com>
2021-09-06 15:30:26 +0100

    back

---
+++ baha/controller/produitC.php
@@ -0,0 +1,60 @@
+<?php
+	include_once "../../config.php";
+	require_once '../../model/produit.php';
+
+
+class produitC {
+   
+    function afficherproduit()
+    {
+        $sql="SElECT * From produit";
+        $db = config::getConnexion();
+        try
+        {
+        $liste=$db->query($sql);
+        return $liste;
+        }
+        catch (Exception $e)
+        {
+            die('Erreur: '.$e->getMessage());
+        }   
+    }
+
+    function ajouterproduit($produit){
+        $sql="INSERT INTO produit (categorie,nom,description,prix,image) 
+        VALUES (:categorie,:nom,:description,:prix,:image)";
+        $db = config::getConnexion();
+        try{
+            $query = $db->prepare($sql);
+        
+            $query->execute([
+                'categorie' => $produit->getcategorie(),
+                'nom' => $produit->getnom(),
+                'description' => $produit->getdescription(),
+                'prix' => $produit->getprix(),
+                'image' => $produit->getimage()
+
+
+
+                
+            ]);			
+        }
+        catch (Exception $e){
+            echo 'Erreur: '.$e->getMessage();
+        }			
+    }
+
+   
+   
+
+   
+    		
+    
+   
+
+
+}
+
+	
+
+?>
